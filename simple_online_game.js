8a272dca7a3b71a05e635c28d0e46b0db07b6e6d
Kim Woo Yong <kwy2000@lycos.co.kr>
2015-12-12 15:37:38 +0900

    Update simple_online_game.js

--- simple_online_game.js
+++ simple_online_game.js
@@ -3,8 +3,7 @@
 	
 	var
 	CHARACTER_SIZE = 96,
-	
-	initialize, sog, Game, Server, Sprite, Painter, Actors, PainterFactory,
+	initialize, painters, sog, Game, Server, Sprite, Painter, Actors, PainterFactory,
 	
 	moveUp = new Image( CHARACTER_SIZE, CHARACTER_SIZE ),
 	moveDown = new Image( CHARACTER_SIZE, CHARACTER_SIZE ),
@@ -54,6 +53,67 @@
 	]
 	;
 	
+	function createPlayerPainters() {
+		var painters = {};
+		
+		for ( var p in sog.sprite ) {
+			painters[ p ] = {
+				UP : {
+					STAY : PainterFactory.create( PainterFactory.UP ),
+					MOVE : PainterFactory.create( PainterFactory.UP ),
+					ATTACK : PainterFactory.create( PainterFactory.ATTACK )
+				},
+				DOWN : {
+					STAY : PainterFactory.create( PainterFactory.DOWN ),
+					MOVE : PainterFactory.create( PainterFactory.DOWN ),
+					ATTACK : PainterFactory.create( PainterFactory.ATTACK )
+				},
+				LEFT : {
+					STAY : PainterFactory.create( PainterFactory.LEFT ),
+					MOVE : PainterFactory.create( PainterFactory.LEFT ),
+					ATTACK : PainterFactory.create( PainterFactory.ATTACK )
+				},
+				RIGHT : {
+					STAY : PainterFactory.create( PainterFactory.RIGHT ),
+					MOVE : PainterFactory.create( PainterFactory.RIGHT ),
+					ATTACK : PainterFactory.create( PainterFactory.ATTACK )
+				}
+			};
+		}
+		
+		return painters;
+	}
+	
+	function setSpriteData( $sprite, $data ) {
+		var painter = painters[ $sprite === sog.sprite.p1 ? 'p1' : 'p2' ][ $data.direction ][ $data.status ];
+		
+		if ( $sprite.painter !== painter ) {
+			$sprite.painter = painter;
+		}
+		if ( $data.status === 'MOVE' || $data.status === 'ATTACK' ) {
+			setAllActors( $sprite );
+		} else {
+			clearAllActors( $sprite );
+		}
+		if ( $sprite === sog.sprite.p1 ) {
+			sog.server.update( { speedV : $data.speedV, speedH : $data.speedH } );
+		}
+	}
+	
+	function setAllActors( $sprite ) {
+		for ( var name in Actors ) {
+			if ( !( name in $sprite.actors ) ) {
+				$sprite.actors[ name ] = new Actors[ name ];
+			}
+		}
+	}
+	
+	function clearAllActors( $sprite ) {
+		for ( var name in Actors ) {
+			delete $sprite.actors[ name ];
+		}
+	}
+	
 	Game = function( $params ) {
 		this.context = $params.context;
 		this.server = $params.server;
@@ -65,6 +125,7 @@
 	};
 	
 	Game.prototype.start = function() {
+		painters = createPlayerPainters();
 		this.server.register( this.room, this.uid );
 		requestAnimationFrame( $util.fn( this.progress, this ) );
 	};
@@ -78,8 +139,10 @@
 
 		if ( data.p2 ) {
 			if ( !this.sprite.p2 ) {
-				this.sprite.p2 = new Sprite( PainterFactory.create( PainterFactory.DOWN ) );
+				this.sprite.p2 = new Sprite;
 			}
+			
+			setSpriteData( this.sprite.p2, data.p2 );
 			this.sprite.p2.update( data.p2, $time );
 			this.sprite.p2.paint( this.context );
 		}
@@ -91,12 +154,21 @@
 		this.host = $params.host;
 		this.dataUrl = $params.dataUrl;
 		this.registerUrl = $params.registerUrl;
-		
-		this.tmp = {};
+		this.characterData = {};
 	};
 	
 	Server.prototype.data = function() {
-		return this.tmp;
+		if ( !sog.sprite.p2 ) {
+			this.characterData.p2 = { speedV : 1, speedH : 0, direction : 'RIGHT', status : 'MOVE' };
+		}
+		if ( sog.sprite.p2 && sog.sprite.p2.left > sog.context.canvas.width - 120 ) {
+			this.characterData.p2 = { speedV : -1, speedH : 0, direction : 'LEFT', status : 'MOVE' };
+		}
+		if ( sog.sprite.p2 && sog.sprite.p2.left < 20 ) {
+			this.characterData.p2 = { speedV : 1, speedH : 0, direction : 'RIGHT', status : 'MOVE' };
+		}
+		
+		return this.characterData;
 	};
 	
 	Server.prototype.register = function( $room, $uid ) {
@@ -104,12 +176,12 @@
 	};
 	
 	Server.prototype.update = function( $data ) {
-		this.tmp.p1 = $data;
+		this.characterData.p1 = $data;
 	};
 	
 	Sprite = function( $painter, $actors ) {
 		this.painter = $painter;
-		this.actors = $actors || [];
+		this.actors = $actors || {};
 		this.left = 0;
 		this.top = 0;
 	};
@@ -119,8 +191,8 @@
 	};
 	
 	Sprite.prototype.update = function( $data, $time ) {
-		for ( var i in this.actors ) {
-			this.actors[ i ].execute( this, $data, $time );
+		for ( var name in this.actors ) {
+			this.actors[ name ].execute( this, $data, $time );
 		}
 	};
 	
@@ -152,24 +224,31 @@
 		}
 	};
 	
-	Actors = {
-		move : {
-			execute : function( $sprite, $data, $time ) {
-				$sprite.left += $data.speedLeft;
-				$sprite.top += $data.speedTop;
+	Actors = (function() {
+		var move, next;
+		
+		move = function() {};
+		move.prototype.execute = function( $sprite, $data, $time ) {
+			$sprite.left += $data.speedV;
+			$sprite.top += $data.speedH;
+		};
+		
+		next = function() {
+			this.interval = 20;
+			this.lastTime = 0;
+		};
+		next.prototype.execute = function( $sprite, $data, $time ) {
+			if ( $time - this.lastTime > this.interval ) {
+				this.lastTime = $time;
+				$sprite.advance();
 			}
-		},
-		next : {
-			interval : 20,
-			lastTime : 0,
-			execute : function( $sprite, $data, $time ) {
-				if ( $time - this.lastTime > this.interval ) {
-					this.lastTime = $time;
-					$sprite.advance();
-				}
-			}
-		}
-	};
+		};
+		
+		return {
+			move : move,
+			next : next
+		};
+	})();
 	
 	PainterFactory = {
 		UP : 'UP',
@@ -201,45 +280,22 @@
 	initialize = function() {
 		var
 		context = document.getElementById( 'canvas' ).getContext( '2d' ),
-		keyInfos = {
-			'38' : { painter : PainterFactory.create( PainterFactory.UP ), speedLeft : 0, speedTop : -1, status : 'MOVE' },
-			'40' : { painter : PainterFactory.create( PainterFactory.DOWN ), speedLeft : 0, speedTop : 1, status : 'MOVE' },
-			'37' : { painter : PainterFactory.create( PainterFactory.LEFT ), speedLeft : -1, speedTop : 0, status : 'MOVE' },
-			'39' : { painter : PainterFactory.create( PainterFactory.RIGHT ), speedLeft : 1, speedTop : 0, status : 'MOVE' },
-			'32' : { painter : PainterFactory.create( PainterFactory.ATTACK ), speedLeft : 0, speedTop : 0, status : 'ATTACK' }
+		keyInfo = {
+			'38' : { speedV : 0, speedH : -1, direction : 'UP', status : 'MOVE' },
+			'40' : { speedV : 0, speedH : 1, direction : 'DOWN', status : 'MOVE' },
+			'37' : { speedV : -1, speedH : 0, direction : 'LEFT', status : 'MOVE' },
+			'39' : { speedV : 1, speedH : 0, direction : 'RIGHT', status : 'MOVE' },
+			'32' : { speedV : 0, speedH : 0, direction : 'DOWN', status : 'ATTACK' }
 		};
 		
 		document.addEventListener( 'keydown', function( $event ) {
-			var	keyInfo, painter, p1 = sog.sprite.p1;
-			
-			if ( $event.keyCode in keyInfos ) {
-				keyInfo = keyInfos[ $event.keyCode ];
-				
-				if ( p1.painter !== keyInfo.painter ) {
-					p1.painter = keyInfo.painter;
-				}
-				if ( p1.actors.indexOf( Actors.next ) === -1 ) {
-					p1.actors.push( Actors.next );
-				}
-				if ( p1.actors.indexOf( Actors.move ) === -1 ) {
-					p1.actors.push( Actors.move );
-				}
-				
-				sog.server.update( {
-					speedLeft : keyInfo.speedLeft,
-					speedTop : keyInfo.speedTop
-				} );
+			if ( $event.keyCode in keyInfo ) {
+				setSpriteData( sog.sprite.p1, keyInfo[ $event.keyCode ] );
 			}
 		}, false );
 		
 		document.addEventListener( 'keyup', function( $event ) {
-			var i, k;
-			
-			for ( k in Actors ) {
-				if ( ( i = sog.sprite.p1.actors.indexOf( Actors[ k ] ) ) > -1 ) {
-					sog.sprite.p1.actors.splice( i, 1 );
-				}
-			}
+			clearAllActors( sog.sprite.p1 );
 		}, false );
 		
 		moveUp.src = 'img/toBack.png';
@@ -250,7 +306,6 @@
 		
 		$util.syncOnLoad( [moveUp, moveDown, moveLeft, moveRight], function() {
 			var server = new Server( { host : 'http://127.0.0.1', dataUrl : '/data', registerUrl : '/register' } );
-			
 			sog = new Game( { context : context, server : server, room : '', uid : '', sprite : new Sprite( PainterFactory.create( PainterFactory.DOWN ) ) } );
 			
 			document.removeEventListener( 'DOMContentLoaded', initialize, false );
